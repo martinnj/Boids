c4199b0759d7286e0421a1371b1189219b2851ac
martinnj <martinnjorgensen@gmail.com>
2013-04-14 12:06:18 +0200

    Added dummy interface for geometry(obstacles). Added base container functionality to world.

--- Boids/Datatypes/Boids/World.cs
+++ Boids/Datatypes/Boids/World.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Datatypes.Geometry;
 
 namespace Datatypes.Boids
 {
     class World
     {
+        #region "Variables"
+
+        public List<Boid> Boids { get; set; }
+        public List<IGeometry> Obstacles { get; set; }
+
+        #endregion
     }
 }
