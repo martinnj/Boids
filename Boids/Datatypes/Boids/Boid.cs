5bedddfa22e99271d67686d8365fd70d71719755
martinnj <martinnjorgensen@gmail.com>
2013-04-14 14:36:45 +0200

    Finished boid classbasic properties.

--- Boids/Datatypes/Boids/Boid.cs
+++ Boids/Datatypes/Boids/Boid.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Datatypes.Math;
 
 namespace Datatypes.Boids
 {
-    /* Implements a simple boid.
-     * A boid is a bird like creature that behavs based on three rules:
-     * 0. Coherence - Wants to stay with other boids of the same type.
-     * 1. Alignment - Wants to fly the same direction as it's flock.
-     * 2. Seperation - Tries not to collide with other boids.
-     * 
-     * This boids model will, when finished, be slightly different in the aspect that boids
-     * will have a "predation level" implementing a simple food-chain where boids can and will
-     * hunt everything
+    /* Implements a simple boid
+     * with basic properties.
      */
     class Boid
     {
         #region "Properties"
+
+        public Vector Position;
+        public Vector Velocity;
+        public int    PredationLevel;
+        //TODO: Implement in later model.
+        // public int    HungerLevel;
+
         #endregion
     }
 }
